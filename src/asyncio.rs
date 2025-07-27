d3aa636df5cc3a09bc297eb076c664b060f666ba
Markus Sintonen <markus.sintonen@gmail.com>
2025-07-27 22:04:58 +0300

    gil once cell

--- src/asyncio.rs
+++ src/asyncio.rs
@@ -1,5 +1,4 @@
 use futures_util::FutureExt;
-use once_cell::sync::OnceCell;
 use pyo3::exceptions::PyRuntimeError;
 use pyo3::prelude::*;
 use pyo3::sync::GILOnceCell;
@@ -87,22 +86,21 @@ impl TaskCallback {
 }
 
 pub struct EventLoopCell {
-    cell: OnceCell<Py<PyAny>>,
+    cell: GILOnceCell<Py<PyAny>>,
 }
 impl EventLoopCell {
     pub fn new() -> Self {
-        EventLoopCell { cell: OnceCell::new() }
+        EventLoopCell {
+            cell: GILOnceCell::new(),
+        }
     }
 
     pub fn get_running_loop(&self, py: Python) -> PyResult<&Py<PyAny>> {
-        self.cell.get_or_try_init(|| Ok(get_running_loop(py)?.unbind()))
+        self.cell.get_or_try_init(py, || Ok(get_running_loop(py)?.unbind()))
     }
 
     pub fn clone(&self, py: Python) -> Self {
-        let cell = match self.cell.get() {
-            Some(value) => OnceCell::with_value(value.clone_ref(py)),
-            None => OnceCell::new(),
-        };
+        let cell = self.cell.clone_ref(py);
         EventLoopCell { cell }
     }
 }
